122b2a46ea85603777e06ca853d3a6429865a2c0
zhuwang <zhuwang@nicomama.com>
2019-05-18 16:48:01 +0800

    u

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# sendto
+# 说明
+一键将当前md文件复制到指定目录。
 
-一键将当前文件复制到指定目录。
+# 用法
 
+因为我用vscode管理markdown文件，有些文件需要放到hexo静态博客里面，不想每次都手动复制过去，所以做了这个扩展。
+
+在vscode中使用时，`ctr+p`启动命令选择班，输入`sendto`，回车即可。
+
+# 配置
+在settings.json编辑，例如
+```
+    "sendto":{
+        "target":"<目标目录>",
+        "meta":true,//是否开启meta检查
+    }
+```
+
+**target：目标目录**
+
+比如我的就是`<hexo笔记项目>/source/_posts`。
+
+
+**meta：true|false,开启则会检查并启动生成meta信息**
+
+meta信息是md内容最前面的一段描述信息，用来给hexo框架生成html的。类似这样：
+```
+---
+title: 文章标题
+date: 2019-05-19
+tags: 
+---
+```
+
+我自己的笔记是不包含meta信息的，复制到hexo笔记是自动给它创建。
+
+
+-END-
--- extension.js
+++ extension.js
@@ -3,6 +3,16 @@
 const vscode = require('vscode');
 const cp = require('child_process')
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const fecha = require("fecha")
+
+
+// 我的日志函数。省的每次都写两行代码啊
+function mylog(info){
+	console.log(info);
+	vscode.window.showInformationMessage(info);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,50 +35,97 @@ function activate(context) {
 		const currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
 
 		if (!currentlyOpenTabfilePath || /^Untitled\-/.test(currentlyOpenTabfilePath)) {
-			console.log(`请先打开一个文件`);
-			vscode.window.showInformationMessage(`请先打开一个文件`);
+			mylog(`请先打开一个文件`);
 		}else{
 			const workbenchConfig = vscode.workspace.getConfiguration('sendto')
 			if (!workbenchConfig){
-				console.log(`sendto配置缺失`);
-				vscode.window.showInformationMessage(`sendto配置缺失`);
+				mylog(`sendto配置缺失`);
 			}else{
 				const target = workbenchConfig.get('target')
 				
 				// check target exist and is dir.
 				if(!target){
-					console.log(`sendto.target配置缺失`);
-					vscode.window.showInformationMessage("sendto.target配置缺失");
+					mylog(`sendto.target配置缺失`);
 				}else{
 					fs.stat(target, (err, stats) => {
 						if (err) {
-							console.log(`${target} ${err ? '不存在' : '存在'}`);
-							vscode.window.showInformationMessage(`${target} ${err ? '不存在' : '存在'}`);
+							mylog(`${target} ${err ? '不存在' : '存在'}`);
 						} else if (!stats.isDirectory()){
-							console.log(`${target} 不是目录`);
-							vscode.window.showInformationMessage(`${target} 不是目录`);
+							mylog(`${target} 不是目录`);
 						}else{
-							let cmd = "cp " + currentlyOpenTabfilePath + " " + target;
-							cp.exec(cmd, (err, stdout, stderr) => {
-								if (err) {
-									console.log(err)
-									vscode.window.showInformationMessage(err);
-								} else {
-									console.log(`当前文章复制到${target},success`)
-									vscode.window.showInformationMessage(`当前文章复制到${target},success`);
-								}
-							});
+							//配置中开启了meta检查。开启，则要确保文章有元信息。
+							if(workbenchConfig.get('meta')){
+								insureHasMeta(currentlyOpenTabfilePath,(err)=>{
+									if(err){
+										mylog(err);
+									}else{
+										doSend(currentlyOpenTabfilePath,target)
+									}
+								})
+							}else{
+								doSend(currentlyOpenTabfilePath,target)
+							}
 						}
 					})
 				}
 			}
-			
-
 		}
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+/**
+ * 确保文章有元信息。没有，则构建并添加到开头
+ * @param  {[type]}   filePath [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function insureHasMeta(filePath, callback=()=>{}) {
+    fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) {
+            callback(err)
+            return 
+        }
+
+        //如果不包含元信息，需要构建并写在开头
+        if (!(/^\-\-\-[^]*[\r\n]title:[^]*[\r\n]\-\-\-/).test(data)) {
+            let meta = [
+                "---",
+                "title: " + path.basename(filePath, ".md"),
+                "date: " + fecha.format(Date.now(), 'YYYY-MM-DD'),
+                "tags: ",
+                "---",
+            ].join(os.EOL)
+
+			fs.writeFile(filePath, meta + os.EOL + data, (err) => {
+                if (err) {
+                	callback(err)
+                }else{
+                	callback(null)
+                }
+            });
+        }
+    })
+}
+
+/**
+ * 复制文件到目录
+ * @param  {[type]} filePath [description]
+ * @param  {[type]} target   [description]
+ * @return {[type]}          [description]
+ */
+function doSend(filePath,target){
+	let cmd = "cp " + filePath + " " + target;
+	cp.exec(cmd, (err, stdout, stderr) => {
+		if (err) {
+			mylog(err)
+		} else {
+			mylog(`成功复制当前文件到${target}`)
+		}
+	});
+}
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
